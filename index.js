b20a845c3b7cc314539d143e92147b233a6bc83f
malik LAFIA <maliksero.lafia@gmail.com>
2024-08-25 11:49:50 +0200

    task3 finished

--- index.js
+++ index.js
@@ -19,3 +19,30 @@ btn.addEventListener("click", () => {
   let textElement = document.getElementById("myText"); //recover the paragraph with id "my test" when the button is clicked
   textElement.textContent = "Hello Check!"; //change the current text to "Hello Check!"
 });
+
+/*TASK 3 | Basic Fetch*/
+async function fetchData() {
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/todos"); //GET request to take the elements from the API
+    let todos = await res.json(); //convert data into json when received
+    return todos; //return all the datas
+  } catch (error) {
+    console.error("Error while recovering the todos: ", error);
+  }
+}
+
+async function displayTodos() {
+  let myList = document.getElementById("todoList"); //Ask the DOM to take the unordored list element
+  let todos = await fetchData(); //call the function fetchData to get the todos list
+
+  //Get and display the five first todos
+  for (let i = 0; i < 5; i++) {
+    let todo = todos[i]; //get a todo element
+    let listElement = document.createElement("li"); //create a li element
+    listElement.textContent = todo.title; //display the title of the todo object
+
+    myList.appendChild(listElement); //add the li element to the unordored list.
+  }
+}
+
+displayTodos(); //call the function to execute the code.
